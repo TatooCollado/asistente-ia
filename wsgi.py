c5213ec2cd11f5d2ecf8158ca825ace69bae9291
Tato <ignaciocollado@hotmail.com>
2025-05-21 17:02:53 -0300

    asdhjausghduashg

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from app import app
+import sys
+import os
 
-if __name__ == "__main__":
-    app.run()
+# Ruta al proyecto
+project_home = '/home/asistenteia/asistente-ia'
+
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+from app import app as application  # Importa tu app Flask
+
+# Si necesitás configurar variables de entorno:
+os.environ['GEMINI_API_KEY'] = 'AIzaSyAFD_0WHxXW44a_QPBswzYp9K0YpW0XZMQ'  # Mejor si lo seteás en el panel AlwaysData

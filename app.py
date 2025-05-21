30ce23a4378f9e3cbba261eee2bb6744842ea634
Tato <ignaciocollado@hotmail.com>
2025-05-21 15:16:12 -0300

    sigo sin poder montarlo online

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def preguntar():
     except Exception as e:
         return jsonify({"error": f"Error al comunicarse con Gemini: {str(e)}"}), 500
 
-# NO USAR app.run() EN PYTHONANYWHERE
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
